0877991d48e89cb19174af95ce9415c46d0ce19c
zhangshuhang <zhangshuhang@zsh6.com>
2022-06-01 18:23:03 +0800

    修复 启动报错问题

--- zsh-cloud-ops/zsh-cloud-monitor-server/src/main/java/com/zsh/cloud/monitor/MonitorServerApplication.java
+++ zsh-cloud-ops/zsh-cloud-monitor-server/src/main/java/com/zsh/cloud/monitor/MonitorServerApplication.java
@@ -3,8 +3,10 @@ package com.zsh.cloud.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MonitorServerApplication {
     
